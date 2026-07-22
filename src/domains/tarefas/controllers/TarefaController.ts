e6d55a48b281513410a2902d943cbfdce72adee0
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-22 12:10:05 -0300

    fix: permite atualizar complted para false

--- src/domains/tarefas/controllers/TarefaController.ts
+++ src/domains/tarefas/controllers/TarefaController.ts
@@ -44,7 +44,7 @@ class TarefaController {
 
     const { title, description, completed } = body;
 
-    if (!title && !description && !completed)
+    if (!title && !description && completed === undefined)
       return res.status(400).json({
         erro: "Body deve conter ao menos um dos campos de Tarefa a ser atualizado",
       });
