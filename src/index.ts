ea2345a99baf4ff5eeb836f56684e61dbdc69127
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-23 11:51:50 -0300

    style: renomeia a pasta handles para middlewares e erros.ts para error.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { tarefaRoutes } from "./routes/tarefa.routes";
-import { ErrorHandler } from "./handlers/erros";
+import { ErrorHandler } from "./middlewares/error";
 
 const app = express();
 const PORTA = 3333;
@@ -8,7 +8,7 @@ const PORTA = 3333;
 app.use(express.json());
 
 app.use("/tarefas", tarefaRoutes);
-app.use(ErrorHandler)
+app.use(ErrorHandler);
 
 app.listen(PORTA, () => {
   console.log(`🚀 Servidor rodando na porta ${PORTA}`);
