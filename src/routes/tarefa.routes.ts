861a8b4704ab01df9da54a0fb68f23b8e06871ca
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-29 11:31:13 -0300

    refactor: adiciona validação de tipo e verificação e de parâmetros obrigatórios ao vlidateBody

--- src/routes/tarefa.routes.ts
+++ src/routes/tarefa.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { TarefaController } from "../domains/tarefas/controllers/TarefaController";
 import validateBody from "../middlewares/validation";
+import { taskKeysFiltered } from "../helpers/const";
 
 const tarefaRoutes = Router();
 const controller = new TarefaController();
@@ -10,9 +11,13 @@ tarefaRoutes.get("/:id", controller.getById);
 
 tarefaRoutes.head("/:id", controller.idExist);
 
-tarefaRoutes.post("/", validateBody, controller.create);
+tarefaRoutes.post(
+  "/",
+  validateBody({ title: true, description: false, completed: false }),
+  controller.create,
+);
 
-tarefaRoutes.put("/:id", validateBody, controller.update);
+tarefaRoutes.put("/:id", validateBody(), controller.update);
 
 tarefaRoutes.delete("/:id", controller.delete);
 
